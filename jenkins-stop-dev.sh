c5c7e21c80cfe15612f94b6001e559bfeb334eca
Rubén Jiménez Goñi <ruben.jimenez@gft.com>
2017-07-31 10:08:07 -0600

    Image name also changed in stop script

--- jenkins-stop-dev.sh
+++ jenkins-stop-dev.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 
-IMAGE_NAME='com.gft/employeeAppraisal:0.0.1-SNAPSHOT'
+IMAGE_NAME='com.gft.employee-appraisal:latest'
 echo 'Stopping containers based on' $IMAGE_NAME
 
 echo 'Searching for previously running containers...'
