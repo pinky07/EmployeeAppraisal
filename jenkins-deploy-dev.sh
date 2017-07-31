c41e86fc3390155e1d423d1767355df4fb31ab21
Rubén Jiménez Goñi <ruben.jimenez@gft.com>
2017-07-31 09:49:14 -0600

    Changed Port

--- jenkins-deploy-dev.sh
+++ jenkins-deploy-dev.sh
@@ -4,8 +4,9 @@
 IMAGE_NAME='com.gft/employeeAppraisal:0.0.1-SNAPSHOT'
 echo 'Launching new container based on image' $IMAGE_NAME
 
-CONTAINER_ID=`docker run -e 'SPRING_PROFILES_ACTIVE=default,dev,oauth2' -d -p 11003:11003 $IMAGE_NAME`
+CONTAINER_ID=`docker run -e 'SPRING_PROFILES_ACTIVE=default,dev,oauth2' -d -p 11004:11004 $IMAGE_NAME`
 echo 'Created container with ID' $CONTAINER_ID
+
 echo 'Waiting 30s'
 sleep 30s
 docker logs $CONTAINER_ID
