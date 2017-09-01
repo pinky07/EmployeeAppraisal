29266bcd364d35028ee0a3ba5bdbd5c715d03892
Rubén Jiménez Goñi <ruben.jimenez@gft.com>
2017-09-01 12:32:20 -0600

    QA deploy script now builds qa db image

--- jenkins/qa-2-deploy.sh
+++ jenkins/qa-2-deploy.sh
@@ -4,13 +4,16 @@
 IMAGE_DB='com.gft.employee-appraisal.db.qa:latest'
 IMAGE_APP='com.gft.employee-appraisal.qa:latest'
 
+echo 'Building new DB image ' ${IMAGE_DB} '...'
+docker build -t ${IMAGE_DB} db/.
+
 echo 'Launch new DB container based on' ${IMAGE_DB} '...'
 docker run \
     -e POSTGRES_USER='postgres' \
     -e POSTGRES_PASSWORD='postgres' \
     -e POSTGRES_DB='employeeappraisals' \
     -d \
-    -p 11025:5432 \
+    -p 11005:5432 \
     ${IMAGE_DB}
 
 echo 'Allowing DB container to start by waiting 20s...'
@@ -18,9 +21,9 @@ sleep 20s
 
 echo 'Launching new APP container based on image' ${IMAGE_APP} '...'
 docker run \
-    -e SPRING_PROFILES_ACTIVE='default,oauth2,qa' \
+    -e SPRING_PROFILES_ACTIVE='default,oauth2,dev' \
     -d \
-    -p 11024:11024 \
+    -p 11004:11004 \
     ${IMAGE_APP}
 
 echo 'Waiting 20s for APP container to start...'
