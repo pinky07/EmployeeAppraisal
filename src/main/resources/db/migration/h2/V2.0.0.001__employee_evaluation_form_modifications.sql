9f51b7890be84f6871e1e97aae9eef35751e6428
Rubén Jiménez Goñi <ruben.jimenez@gft.com>
2017-09-11 14:55:48 -0600

    Uncommented EmployeeEvaluationForm seeds

--- src/main/resources/db/migration/h2/V2.0.0.001__employee_evaluation_form_modifications.sql
+++ src/main/resources/db/migration/h2/V2.0.0.001__employee_evaluation_form_modifications.sql
@@ -42,30 +42,34 @@ ALTER TABLE Section ADD CONSTRAINT Section_scoreTypeId_fkey FOREIGN KEY (scoreTy
 ALTER TABLE EmployeeEvaluationForm DROP COLUMN employeeRelationshipId;
 ALTER TABLE EmployeeEvaluationForm DROP COLUMN evaluationStatus;
 
--- Insert new Data
+-- Delete old data
 
---INSERT INTO EmployeeEvaluationForm (id, appraisalXEvaluationFormId, employeeId, filledByEmployeeId, mentorId, createDate, submitDate, evaluationStatus)
---VALUES
---
---  -- Rubén Jiménez (id 2) - Self Evaluation
---  (1, 1, 2, 2, 6, '2017-01-01 00:00:00-06', null, 'PENDING'),
---
---  -- Rubén Jiménez (id 2) - Reference Evaluation
---  (2, 2, 2, 3, 6, '2017-01-01 00:00:00-06', null, 'PENDING'),
---  (3, 2, 2, 4, 6, '2017-01-01 00:00:00-06', null, 'PENDING'),
---  (4, 2, 2, 5, 6, '2017-01-01 00:00:00-06', null, 'PENDING'),
---
---  -- Rubén Jiménez (id 2) - Mentor Evaluation
---  (5, 3, 2, 6, 6, '2017-01-01 00:00:00-06', null, 'PENDING'),
---
---  -- Manuel Yépez (id 3) - Self Evaluation
---  (6, 1, 3, 3, 6, '2017-01-01 00:00:00-06', null, 'PENDING'),
---
---  -- Manuel Yépez (id 3) - Reference Evaluation
---  (7, 2, 3, 2, 6, '2017-01-01 00:00:00-06', null, 'PENDING'),
---  (8, 2, 3, 4, 6, '2017-01-01 00:00:00-06', null, 'PENDING'),
---  (9, 2, 3, 5, 6, '2017-01-01 00:00:00-06', null, 'PENDING'),
---
---  -- Manuel Yépez (id 3) - Mentor Evaluation
---  (10, 3, 3, 6, 6, '2017-01-01 00:00:00-06', null, 'PENDING');
+DELETE FROM EmployeeEvaluationForm;
+
+-- Insert new data
+
+INSERT INTO EmployeeEvaluationForm (id, appraisalXEvaluationFormTemplateId, employeeId, filledByEmployeeId, mentorId, createDate, submitDate)
+VALUES
+
+  -- Rubén Jiménez (id 2) - Self Evaluation
+  (1, 1, 2, 2, 6, '2017-01-01 00:00:00-06', null),
+
+  -- Rubén Jiménez (id 2) - Reference Evaluation
+  (2, 2, 2, 3, 6, '2017-01-01 00:00:00-06', null),
+  (3, 2, 2, 4, 6, '2017-01-01 00:00:00-06', null),
+  (4, 2, 2, 5, 6, '2017-01-01 00:00:00-06', null),
+
+  -- Rubén Jiménez (id 2) - Mentor Evaluation
+  (5, 3, 2, 6, 6, '2017-01-01 00:00:00-06', null),
+
+  -- Manuel Yépez (id 3) - Self Evaluation
+  (6, 1, 3, 3, 6, '2017-01-01 00:00:00-06', null),
+
+  -- Manuel Yépez (id 3) - Reference Evaluation
+  (7, 2, 3, 2, 6, '2017-01-01 00:00:00-06', null),
+  (8, 2, 3, 4, 6, '2017-01-01 00:00:00-06', null),
+  (9, 2, 3, 5, 6, '2017-01-01 00:00:00-06', null),
+
+  -- Manuel Yépez (id 3) - Mentor Evaluation
+  (10, 3, 3, 6, 6, '2017-01-01 00:00:00-06', null);
 
